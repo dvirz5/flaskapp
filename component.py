4084715084719b68a6b7bf623fe2c1823d3e3c8c
dvir <dvirz.point@gmail.com>
2018-04-03 18:13:10 +0300

    1

--- component.py
+++ component.py
@@ -1,14 +1,6 @@
-from flaskext.mysql import MySQL
-from flask import Flask,request
-from flask import json
-import sys
-from flask import Flask, render_template, json, request
-
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-def welcome():
-    return render_template('index.html')
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def hello():
+    return "Hello World!"
